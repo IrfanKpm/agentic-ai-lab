8a268f2885dcc49e9dc14f9eaa07d920ccbe8497
IrfanKpm <muhammedirfanp002@gmail.com>
2026-05-14 18:23:04 +0530

    implement basic agent tools with wikipedia and vector retriever

--- agents/main.py
+++ agents/main.py
@@ -1,6 +1,15 @@
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_core.tools.retriever import create_retriever_tool
+import settings
 
+
+# src1
 api_wrapper = WikipediaAPIWrapper(
     top_k_results=1,
     doc_content_chars_max=200
@@ -10,3 +19,39 @@ tool = WikipediaQueryRun(api_wrapper=api_wrapper)
 
 result = tool.run("LangChain")
 print(result)
+
+
+# src2
+gfg_url = "https://www.geeksforgeeks.org/artificial-intelligence/agents-artificial-intelligence/"
+
+loader = WebBaseLoader(gfg_url)
+docs = loader.load()
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+documents = splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vectorDB = FAISS.from_documents(documents, embeddings)
+retriever = vectorDB.as_retriever()
+
+retriever_tool = create_retriever_tool(
+    retriever,
+    "ai_agents_tool",
+    """
+    Use this tool for Artificial Intelligence Agents concepts.
+
+    Includes:
+    - What are AI agents
+    - Types of agents in AI
+    - Agent architectures
+    - Goal-based agents
+    - Utility-based agents
+    - Learning agents
+    - Real-world agent applications
+    """
+)
+
+response = retriever_tool.invoke("What are AI agents?")
+print(response)

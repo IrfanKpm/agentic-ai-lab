7c2257255d11a95486202710f978d44576a4e90e
IrfanKpm <muhammedirfanp002@gmail.com>
2026-06-01 21:31:33 +0530

    feat: implement single-node chatbot graph using Gemini

--- ch-langgraph/chatbot1/main.py
+++ ch-langgraph/chatbot1/main.py
@@ -12,8 +12,33 @@ import settings
 class State(TypedDict):
     messages: Annotated[list, add_messages]
 
-graph_builder = StateGraph(State)
 
 
-llm = init_chat_model("gpt-4o-mini",model_provider="openai"
+
+llm = init_chat_model(
+    model="gemini-2.5-flash-lite",
+    model_provider="google_genai",
+    temperature=0,
 )
+
+# Node Functionality
+def chatbot(state: State):
+    response = llm.invoke(state["messages"])
+    return {"messages": [response]}
+
+# Graph Setup
+graph_builder = StateGraph(State)
+graph_builder.add_node("llmChatbot",chatbot) # (node_name,node_fucntion)
+graph_builder.add_edge(START,"llmChatbot")
+graph_builder.add_edge("llmChatbot",END)
+
+# Graph Compile
+graph = graph_builder.compile()
+
+# Visualize the Graph
+def visualGraph():
+    image = graph.get_graph().draw_png()
+    with open("graph.png", "wb") as f:
+        f.write(image)
+
+visualGraph()
